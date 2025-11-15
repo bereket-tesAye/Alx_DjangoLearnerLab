3f3d33f9e71dba9d366230f95f7902fcdda6ad26
Your Name <you@example.com>
2025-11-16 01:07:31 +0300

    added a permission

--- advanced_features_and_security/bookshelf/admin.py
+++ advanced_features_and_security/bookshelf/admin.py
@@ -1,15 +1,8 @@
 from django.contrib import admin
 from .models import Book
 
-class BookAdmin(admin.ModelAdmin):
-    # Fields to display in the list view
-    list_display = ('title', 'author', 'publication_year')
-    
-    # Enable search by title or author
-    search_fields = ('title', 'author')
-    
-    # Add filters for publication year
-    list_filter = ('publication_year',)
 
-# Register with the custom admin configuration
-admin.site.register(Book, BookAdmin)
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    # Custom permissions are automatically available
